f7f14a036d72d9f3a50463325604aefc50b2915e
Rudra Prasad Lugun <126277385+RudranotOP@users.noreply.github.com>
2026-01-18 20:13:33 +0530

    Time: 57 ms (25.84%) | Memory: 54.3 MB (65.11%) - LeetSync

---
+++ 2749-promise-time-limit/promise-time-limit.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+    return async function(...args) {
+        return new Promise((resolve, reject) => {
+            const timeoutId = setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+
+            fn(...args)
+                .then((result) => {
+                    clearTimeout(timeoutId);
+                    resolve(result);
+                })
+                .catch((error) => {
+                    clearTimeout(timeoutId);
+                    reject(error);
+                });
+        });
+    };
+};
