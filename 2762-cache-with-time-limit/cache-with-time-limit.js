ff02c6b83e31d6eb7ee81edfb9bfd4459578db14
Rudra Prasad Lugun <126277385+RudranotOP@users.noreply.github.com>
2026-01-18 20:15:30 +0530

    Time: 46 ms (73.83%) | Memory: 53.8 MB (54.27%) - LeetSync

---
+++ 2762-cache-with-time-limit/cache-with-time-limit.js
@@ -0,0 +1,40 @@
+var TimeLimitedCache = function() {
+    this.cache = new Map();
+};
+
+/** * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    const keyExists = this.cache.has(key);
+    
+    if (keyExists) {
+        clearTimeout(this.cache.get(key).timerId);
+    }
+
+    const timerId = setTimeout(() => {
+        this.cache.delete(key);
+    }, duration);
+
+    this.cache.set(key, { value: value, timerId: timerId });
+    
+    return keyExists;
+};
+
+/** * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    if (this.cache.has(key)) {
+        return this.cache.get(key).value;
+    }
+    return -1;
+};
+
+/** * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
