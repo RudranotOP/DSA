d565ff969ee6a93b9cadc6899f329a9458f75cda
Rudra Prasad Lugun <126277385+RudranotOP@users.noreply.github.com>
2026-02-07 23:28:18 +0530

    Time: 21 ms (67.46%) | Memory: 25.7 MB (54.88%) - LeetSync

---
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int b = 0, ans = 0;
+        for (char c : s) {
+            if (c == 'b') b++;
+            else ans = min(ans + 1, b);
+        }
+        return ans;
+    }
+};
